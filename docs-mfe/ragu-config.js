30f248e37cbc27dec41f58899b57274f90a6695c
Maniero <cmaniero@thoughtworks.com>
2020-10-20 02:05:07 -0300

    set project root as the project does not ran from root

--- docs-mfe/ragu-config.js
+++ docs-mfe/ragu-config.js
@@ -3,6 +3,7 @@ const {createReactRaguServerConfig} = require('ragu-react-server-adapter/config'
 const port = parseInt(process.env.PORT || '3100');
 
 const config = createReactRaguServerConfig({
+  projectRoot: __dirname,
   compiler: {
     assetsPrefix: process.env.ASSETS_PREFIX || `http://localhost:${port}/component-assets/`
   },
