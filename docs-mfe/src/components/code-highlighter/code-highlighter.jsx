2c3875a79b73e171048ff9d18aee050b2ef13a76
Maniero <cmaniero@thoughtworks.com>
2020-10-20 15:27:49 -0300

    load syntaxHighlighter async to optmize first load

--- docs-mfe/src/components/code-highlighter/code-highlighter.jsx
+++ docs-mfe/src/components/code-highlighter/code-highlighter.jsx
@@ -1,13 +1,27 @@
-import SyntaxHighlighter from "react-syntax-highlighter";
-import atomDark from "react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-dark";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './code-highlighter.css';
 
 export const CodeHighlighter = ({children, language, filename}) => {
+  const [content, setContent] = useState('loading...');
+
+  useEffect(() => {
+    (async () => {
+      const [{default: SyntaxHighlighter}, {default: atomDark}] = await Promise.all([
+        import('react-syntax-highlighter'),
+        import('react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-dark')
+      ]);
+
+      setContent(<SyntaxHighlighter language={language} style={atomDark} showLineNumbers={true}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    })();
+  }, []);
+
   return (<div className="ragu-react-server-adapter__code-highlighter">
     {filename && <div className="ragu-react-server-adapter__code-highlighter__filename">{filename}</div>}
-    <SyntaxHighlighter language={language} style={atomDark} showLineNumbers={true}>
-      {children}
-    </SyntaxHighlighter>
+    <div>
+      {content}
+    </div>
   </div>);
 }
