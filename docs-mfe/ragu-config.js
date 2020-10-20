01981a98d52b61f3fbca2ac2ed338dad109efc21
Maniero <cmaniero@thoughtworks.com>
2020-10-20 15:05:17 -0300

    adds react dependency

--- docs-mfe/ragu-config.js
+++ docs-mfe/ragu-config.js
@@ -8,7 +8,25 @@ module.exports = createReactRaguServerConfig({
     assetsPrefix: process.env.ASSETS_PREFIX || `http://localhost:${port}/component-assets/`
   },
   components: {
-    namePrefix: 'my_project_name_'
+    namePrefix: 'my_project_name_',
+    defaultDependencies: [
+      {
+        nodeRequire: 'react',
+        globalVariable: 'React',
+        dependency: 'https://cdnjs.cloudflare.com/ajax/libs/react/17.0.0-rc.2/umd/react.production.min.js'
+      },
+      {
+        nodeRequire: 'react-is',
+        globalVariable: 'reactIs',
+        dependency: 'https://cdnjs.cloudflare.com/ajax/libs/react-is/17.0.0-rc.2/umd/react-is.production.min.js'
+      },
+      {
+        nodeRequire: 'react-dom',
+        globalVariable: 'ReactDOM',
+        dependency: 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.0-rc.2/umd/react-dom.production.min.js',
+        order: 1
+      }
+    ]
   },
   server: {
     port
