4e176f210887f16018e9d932819fbe9f5ba0380a
Maniero <carlosmaniero@gmail.com>
2020-10-23 13:41:27 -0300

    removes __dirname as it is not necessary

--- docs-mfe/ragu-config.js
+++ docs-mfe/ragu-config.js
@@ -3,7 +3,6 @@ const {createReactRaguServerConfig} = require('ragu-react-server-adapter/config'
 const port = parseInt(process.env.PORT || '3100');
 
 module.exports = createReactRaguServerConfig({
-  projectRoot: __dirname,
   compiler: {
     assetsPrefix: process.env.ASSETS_PREFIX || `http://localhost:${port}/component-assets/`
   },
