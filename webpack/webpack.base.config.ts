ef19f4e6f78ce591586a56c34f964207ce61ff5e
Maniero <carlosmaniero@gmail.com>
2021-03-06 00:52:08 -0300

    adds react runtime resolution to prevent importing react

--- webpack/webpack.base.config.ts
+++ webpack/webpack.base.config.ts
@@ -21,7 +21,10 @@ export const raguReactWebpackBaseConfig = (config: RaguServerConfig): webpack.Co
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
+              presets: [
+                  '@babel/preset-env',
+                  ["@babel/preset-react", { "runtime": "automatic" }]
+              ],
               plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-modules-commonjs']
             }
           }
