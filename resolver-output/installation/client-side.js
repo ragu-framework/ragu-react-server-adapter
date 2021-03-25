4abddbf1af52195d736f73c234a65af9a921f4cb
carlosmaniero <carlosmaniero@gmail.com>
2021-03-25 03:56:32 +0000

    Deploying to gh-pages from  @ a9fbe6991e6cb61ab9dc7a3dffd03c380b607ea9 🚀

--- resolver-output/installation/client-side.js
+++ resolver-output/installation/client-side.js
@@ -1,6 +1,6 @@
 
       var component = require('/home/runner/work/ragu-react-server-adapter/ragu-react-server-adapter/docs-mfe/ragu-components/installation');
-      var resolver = require('/home/runner/work/ragu-react-server-adapter/ragu-react-server-adapter/docs-mfe/node_modules/ragu-react-server-adapter/resolvers/hydrate-resolver');
+      var resolver = require('/home/runner/work/ragu-react-server-adapter/ragu-react-server-adapter/docs-mfe/node_modules/ragu-react-server-adapter/resolvers/client-side-resolver');
 
       module.exports.default = (resolver.default || resolver)(component.default || component);
     
