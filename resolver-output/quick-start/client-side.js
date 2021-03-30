b95eecd4deecd11865b8f66b92f67819044f9f57
carlosmaniero <carlosmaniero@gmail.com>
2021-03-30 02:28:29 +0000

    Deploying to gh-pages from  @ 380772e0c2229ef327a1d782a60b87e82442143f 🚀

--- resolver-output/quick-start/client-side.js
+++ resolver-output/quick-start/client-side.js
@@ -1,6 +1,8 @@
 
       var component = require('/home/runner/work/ragu-react-server-adapter/ragu-react-server-adapter/docs-mfe/ragu-components/quick-start');
       var resolver = require('/home/runner/work/ragu-react-server-adapter/ragu-react-server-adapter/docs-mfe/node_modules/ragu-react-server-adapter/resolvers/client-side-resolver');
-
-      module.exports.default = (resolver.default || resolver)(component.default || component);
+      var wrapperFn = function (component) {
+      return component;
+    };
+      module.exports.default = (resolver.default || resolver)(component.default || component, wrapperFn);
     
